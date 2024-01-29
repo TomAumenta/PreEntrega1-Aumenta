e50515b9d1b26d9de7eb01c7109400a21dd9ee5c
Tomas Aumenta <tomas.aumenta@gmail.com>
2024-01-28 21:29:55 -0300

    Agregamos bucle ciclo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-
+// conversor de dolar a peso argentino
 
 const valorPeso = 1220; // Tipo de cambio fijo (1 USD = 90.9091 ARS)
 
@@ -12,3 +12,18 @@ if (valorDolar) {
     console.log('Ingreso cancelado o valor inválido.');
 }
 
+
+//A modo de en un futuro poner esto como un limite de tiempo que puede estar inactivo en el sitio
+
+var contador = 0;
+
+while (contador < 120) {
+    console.log("Contador: " + contador);
+
+    if (contador === 100) {
+        console.log("¡El contador llegó a 100 ");
+        break; 
+    }
+
+    contador++;
+}

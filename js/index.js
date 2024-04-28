931f964375c3b73086b24d3388e5dec66f5a3894
Tomas Aumenta <tomas.aumenta@gmail.com>
2024-04-28 15:12:25 -0300

    Conversor

--- js/index.js
+++ js/index.js
@@ -1,29 +1,13 @@
 
 // conversor de dolar a peso argentino
 
-const valorPeso = 1220; // Tipo de cambio fijo (1 USD = 90.9091 ARS)
+const valorPeso = 1220; 
 
-const valorDolar = prompt('Ingrese la cantidad en dólares estadounidenses:');
+const valorDolar = prompt('Ingrese la cantidad en dólares:');
 
 if (valorDolar) {
     const resultado = valorDolar * valorPeso;
-    console.log(`Equivalentes a ${resultado} pesos argentinos`);
+    alert(`Equivalentes a ${resultado} pesos argentinos`);
 } else {
     console.log('Ingreso cancelado o valor inválido.');
-}
-
-
-//A modo de en un futuro poner esto como un limite de tiempo que puede estar inactivo en el sitio
-
-var contador = 0;
-
-while (contador < 120) {
-    console.log("Contador: " + contador);
-
-    if (contador === 100) {
-        console.log("Pasaron 100s de inactividad, por favor recargar la página!");
-        break; 
-    }
-
-    contador++;
 }
